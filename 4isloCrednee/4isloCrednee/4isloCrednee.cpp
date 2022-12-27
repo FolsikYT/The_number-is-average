24683ee1076cbf941d12a8fdbbfd379606b15e67
Patc <106960561+FolsikYT@users.noreply.github.com>
2022-12-27 15:11:16 +0300

    Add files via upload

---
+++ 4isloCrednee/4isloCrednee/4isloCrednee.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a, b, c;
+    cout << "Введите 3 разных числа через пробел: ";
+    cin >> a >> b >> c;
+    if (a < b)
+    {
+        if (a > c)
+        {
+            cout << "Число " << a << " среднее";
+        }
+        else
+        {
+            if (b > c)
+            {
+                cout << "Число " << c << " среднее";
+            }
+            else
+            {
+                cout << "Число " << b << " среднее";
+            }
+        }
+    }
+    
+    cin.get();
+    cin.get();
+    return 0;
+}
